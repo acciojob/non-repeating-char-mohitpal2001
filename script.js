165dba253867c3652c497df7aa2ba09d924ab3d4
mohitpal2001 <91897271+mohitpal2001@users.noreply.github.com>
2024-11-16 13:14:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+	if(str==""||str.length==0){
+		return null;
+	}
+	let freqobj={}
+for(let i =0;i<str.length;i++){
+	if(!freqobj[str.charAt(i)]){
+		freqobj[str.charAt(i)]=1;
+	}else{
+		freqobj[str.charAt(i)]+=1;
+	}
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+	for(let item in freqobj){
+		if(freqobj[item]==1){
+			return item
+		}
+	}
+	return null;
+	
+}
+// const input = prompt("Enter a string");
+// alert(firstNonRepeatedChar(input)); 
